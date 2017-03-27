deecde9e5fbce3b16c0a9d820a8e144c690b6afa
Marcin Zajączkowski <marcin.zajaczkowski@4finance.com>
2017-03-27 16:35:01 +0200

    Backport (optional) XmlUnit per test configuration

--- src/test/groovy/io/fourfinanceit/pipeline/example/util/XmlComparator.groovy
+++ src/test/groovy/io/fourfinanceit/pipeline/example/util/XmlComparator.groovy
@@ -5,6 +5,7 @@ import org.custommonkey.xmlunit.DetailedDiff
 import org.custommonkey.xmlunit.Diff
 import org.custommonkey.xmlunit.ElementNameAndAttributeQualifier
 import org.custommonkey.xmlunit.XMLUnit
+import org.junit.Before
 import org.junit.ComparisonFailure
 
 /**
@@ -15,28 +16,45 @@ import org.junit.ComparisonFailure
 //TODO rethink the way files are accessed in this class
 trait XmlComparator {
 
-    void compareXmls(String fileName, Node nodeToCompare) {
+    private XMLUnit xmlUnit
+
+    @Before
+    void init() {
+        xmlUnit = new XMLUnit()
+        xmlUnit.ignoreWhitespace = true
+        xmlUnit.normalizeWhitespace = true
+    }
+
+    void compareXmls(String file, Node nodeToCompare) {
+        //default parameter initializers are not allowed in traits
+        compareXmls(file, nodeToCompare, false)
+    }
+
+    void compareXmls(String fileName, Node nodeToCompare, boolean displayActualXmlInCaseOfError) {
         String nodeXml = XmlUtil.serialize(nodeToCompare).stripIndent().stripMargin()
         def referenceXmlFile = getFileOrNull(fileName)
         if (!referenceXmlFile) {
             if (System.getProperty('outputMissingXml') == 'true') {
-                new File("./src/test/resources/${fileName}").text = nodeXml
+                def missingXml = new File("./src/test/resources/${fileName}")
+                missingXml.parentFile.mkdirs()
+                missingXml.text = nodeXml
             }
             throw new RuntimeException("Reference xml file [$fileName] not found")
         }
         String referenceXml = XmlUtil.serialize(referenceXmlFile.text).stripIndent().stripMargin()
-        compareXmls(fileName, referenceXml, nodeXml)
+        compareXmls(fileName, referenceXml, nodeXml, displayActualXmlInCaseOfError)
     }
 
-    void compareXmls(String fileName, String referenceXml, String nodeXml) {
-        XMLUnit.setIgnoreWhitespace(true)
-        XMLUnit.setNormalizeWhitespace(true)
-        Diff diff = XMLUnit.compareXML(referenceXml, nodeXml)
+    void compareXmls(String fileName, String referenceXml, String nodeXml, boolean displayActualXmlInCaseOfError) {
+        Diff diff = xmlUnit.compareXML(referenceXml, nodeXml)
         diff.overrideElementQualifier(new ElementNameAndAttributeQualifier())
         if (!diff.identical()) {
             DetailedDiff detailedDiff = new DetailedDiff(diff)
+            if (displayActualXmlInCaseOfError) {
+                println("Actual XML:\n $nodeXml")
+            }
             if (System.getProperty("outputActualXml") == 'true') {
-                new File("./src/test/resources/${fileName}.ACTUAL.xml").text = nodeXml
+                new File("src/test/resources/${fileName}.ACTUAL.xml").text = nodeXml
             }
             throw new XmlsAreNotSimilar(fileName, detailedDiff.allDifferences, referenceXml, nodeXml)
         }
